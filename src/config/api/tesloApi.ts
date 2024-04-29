46c9518295e1e50ecbb76d1b76471151be866781
williamsmata <williams.rm99@gmail.com>
2024-04-29 15:49:23 -0400

    Check status action done

--- src/config/api/tesloApi.ts
+++ src/config/api/tesloApi.ts
@@ -1,6 +1,7 @@
 import {STAGE, API_URL as PROD_URL, API_URL_ANDROID, API_URL_IOS} from '@env';
 import axios from 'axios';
 import {Platform} from 'react-native';
+import {StorageAdapter} from '../adapters/storage-adapter';
 
 export const API_URL =
   STAGE === 'prod'
@@ -17,5 +18,13 @@ const tesloApi = axios.create({
 });
 
 // todo: add interceptors
+
+tesloApi.interceptors.request.use(async config => {
+  const token = await StorageAdapter.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default tesloApi;
--- src/presentation/store/auth/useAuthStore.ts
+++ src/presentation/store/auth/useAuthStore.ts
@@ -1,7 +1,7 @@
 import {create} from 'zustand';
 import type {User} from '../../../domain/entities/user';
 import type {AuthStatus} from '../../../infrastructure/interfaces/auth.status';
-import {authLogin} from '../../../actions/auth/auth';
+import {authCheckStatus, authLogin} from '../../../actions/auth/auth';
 import {StorageAdapter} from '../../../config/adapters/storage-adapter';
 
 export interface AuthState {
@@ -10,6 +10,7 @@ export interface AuthState {
   user: User | null;
 
   login: (email: string, password: string) => Promise<boolean>;
+  checkStatus: () => Promise<boolean>;
 }
 
 export const useAuthStore = create<AuthState>(set => ({
@@ -25,7 +26,20 @@ export const useAuthStore = create<AuthState>(set => ({
       return false;
     }
 
-    // todo: save token in storage
+    await StorageAdapter.setItem('token', resp.token);
+
+    set({status: 'authenticated', token: resp.token, user: resp.user});
+
+    return true;
+  },
+
+  checkStatus: async () => {
+    const resp = await authCheckStatus();
+
+    if (!resp) {
+      set({status: 'unauthenticated', token: null, user: null});
+      return false;
+    }
 
     await StorageAdapter.setItem('token', resp.token);
 
