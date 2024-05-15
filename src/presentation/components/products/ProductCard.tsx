16145f29b65043a9e1918be15267ebeae4d54bd6
williamsmata <williams.rm99@gmail.com>
2024-05-15 10:55:16 -0400

    feat: Add lazy loading for images in ProductCard component

--- src/presentation/components/products/ProductCard.tsx
+++ src/presentation/components/products/ProductCard.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
-// import {StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Product} from '../../../domain/entities/product';
-import {Text} from '@ui-kitten/components';
+import {Image} from 'react-native';
+import {Card, Text} from '@ui-kitten/components';
+import {FadeInImage} from '../ui/FadeInImage';
 
 interface ProductCardProps {
   product: Product;
 }
 
 export default function ProductCard({product}: ProductCardProps) {
-  return <Text>{product.id}</Text>;
+  return (
+    <Card style={styles.card}>
+      {product.images.length > 0 ? (
+        <FadeInImage uri={product.images[0]} style={styles.image} />
+      ) : (
+        <Image
+          source={require('../../../assets/no-product-image.png')}
+          style={styles.notFoundImage}
+        />
+      )}
+
+      <Text numberOfLines={2} style={styles.text} category="h6">
+        {product.title}
+      </Text>
+    </Card>
+  );
 }
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    backgroundColor: '#f9f9f9',
+    margin: 3,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: 200,
+  },
+  notFoundImage: {
+    width: '100%',
+    height: 200,
+  },
+  text: {
+    textAlign: 'center',
+  },
+});
